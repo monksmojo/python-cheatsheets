d6c6559d3f657c15896aff64b0e17eab71e8f458
uditmakkar <makkarudit8@gmail.com>
2019-08-30 21:06:33 +0530

    my first commit

---
+++ python_cheat_sheet.py
@@ -0,0 +1,32 @@
+print("hello world")
+# first python programme to print hello world
+# printing some slashes and symbol
+# using semicolon at the end of every statement is optional
+print("   /|")
+print("  / |")
+print(" /__|")
+
+# variables in python
+
+print("once there was a man name george")
+print("he was 70 years old")
+print("he liked his name very much")
+print("but he didn't like age 70")
+
+# instead of hard coding age and name  we can use a variable to store character name and age;
+
+character_name = "clark"
+character_age = "35"
+print("once there was a man name" + character_name)
+print("he was" + character_age + "years old")
+print("he liked his name very much")
+print("but he didn't like age " + character_age)
+
+# different type of variables
+# string= stream of character forming plain text;
+# number= can contain decimal/float/double valus
+# boolean= can contain true or false
+
+string_name = "clark kent"
+number_age = 10
+bool_is_male = True
