ffa14f1cce64077375f6b103afd95adfaa4c2bf5
uditmakkar <makkarudit8@gmail.com>
2019-09-03 15:33:11 +0530

    "till docstring"

--- Programiz_python_cheatsheet0.py
+++ Programiz_python_cheatsheet0.py
@@ -1,5 +1,32 @@
 # PROGRAMIZ PYTHON TUTORIAL CHEATSHEET
-# 1.INTRODUCTION TO PYTHON
+
+# Python Comments
+# Comments are very important while writing a program. It describes what's going on inside a program so that a person looking at the source code does not have a hard time figuring it out. You might forget the key details of the program you just wrote in a month's time. So taking time to explain these concepts in form of comments is always fruitful.
+
+# In Python, we use the hash
+# (#) symbol to start writing a comment.
+
+# It extends up to the newline character. Comments are for programmers for better understanding of a program. Python Interpreter ignores comment.
+
+# This is a comment
+# print out Hello
+print('Hello')
+
+# Multi-line comments
+# If we have comments that extend multiple lines, one way of doing it is to use hash (#) in the beginning of each line. For example:
+
+# This is a long comment
+# and it extends
+# to multiple lines
+# Another way of doing this is to use triple quotes, either ''' or """.
+
+# These triple quotes are generally used for multi-line strings. But they can be used as multi-line comment as well. Unless they are not docstrings, they do not generate any extra code.
+
+"""This is also a
+perfect example of
+multi-line comments"""
+
+# INTRODUCTION TO PYTHON
 # PYTHON IS A CROSS PLATFORM PROGRAMMING LANGUAGE MEANS IT CAN RUN ON MULTIPLE OS LIKE WINDOWS, MACos & LINUX
 # ITS FREE AND OPEN SOURCE
 # PYTHON FILES ARE ALWAYS CREATED WITH THE .py EXTENSION
@@ -54,10 +81,53 @@ print("hello world!")
 # Instructions that a Python interpreter can execute are called statements. For example, a = 1 is an assignment statement.
 
 # Multi-line statement
-
-# In Python, end of a statement is marked by a newline character. But we can make a statement extend over multiple lines with the line continuation character (\). For example:
-
+# # In Python, end of a statement is marked by a newline character. But we can make a statement extend over multiple lines with the line continuation character (\). For example:
 a = 1+2+3+4 +\
     5+6+7+8 +\
     10
-print("value of a=", a, "boom")
+print("value of a=", a, "using explicit line continuation")
+
+# using \ is the explicit way to show line continuation
+# we can also implement using
+# In Python, line continuation is implied inside parentheses ( ), brackets [ ] and braces { }. For instance, we can implement the above multi-line statement as
+
+a = (1+2+3+4 +
+     5+6 +
+     7+8+10)
+print("value of a=", a, "using explicit line continuation")
+
+# in python its not a good practice to   use ;(semicolon) after every statement.
+
+# in python we use semicolon to put multiple statements in a single line using semicolons, as follows
+
+a = 1
+b = 2
+c = 3
+
+# Python Indentation
+# Most of the programming languages like C, C++, Java use braces { } to define a block of code. Python uses indentation.
+
+# A code block (body of a function, loop etc.) starts with indentation and ends with the first unindented line. The amount of indentation is up to you, but it must be consistent throughout that block.
+
+# Generally four whitespaces are used for indentation and is preferred over tabs. Here is an example.
+
+for i in range(1, 101):
+    print(i)
+    if(i == 5):
+        break
+
+# Docstring in Python
+
+# Docstring is short for documentation string.
+
+# It is a string that occurs as the first statement in a module, function, class, or method definition. We must write what a function/class does in the docstring.
+# Triple quotes are used while writing docstrings. For example:
+
+
+def double_function(num):
+    """Function to double the value"""
+    print(2*num)
+
+
+print(double_function.__doc__, "of 10")
+double_function(10)
